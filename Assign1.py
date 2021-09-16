8996ff1e5ac341f5d59ec6f6beed1f8d9d63bacb
Anos Voldigoad <anosvoldigoad@skid.cyou>
2021-09-16 13:46:29 -0400

    Create Assign1.py

---
+++ Assign1.py
@@ -0,0 +1,32 @@
+"""
+@Author: Harry Vu
+@Description: Calculating cost of electricity
+@Last modified: Sep 16, 2021
+"""
+
+total_usage_discount = 4/100    # 4%
+on_peak_discount = 5/100        # 5%
+senior_discount = 11/100        # 11%
+tax = 13/100                    # 13%
+
+offPP = 0   # Off peak period
+onPP = 0    # On peak period
+mPP = 0     # Mid peak period
+
+# Welcome header
+print("=========================")
+print("   CoE Calculator v1.0   ")
+print("=========================")
+
+# Calculator
+ offPP = input("Enter kwh during Off Peak period: ")
+ onPP = input()
+ 
+
+
+
+
+
+
+
+
